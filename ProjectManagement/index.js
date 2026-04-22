912083b4ac375924a1d4dcd526d82de9255b48b9
Mohammed Saqlain <mdsaqlain2486@gmail.com>
2026-04-22 16:35:57 +0530

    Made some changes after installing dotenv

--- ProjectManagement/index.js
+++ ProjectManagement/index.js
@@ -1,1 +1,16 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env"
+});
+
+let myusername = process.env.Username;
+
+let mydatabase = process.env.Database;
+
+
+console.log("value : " , myusername);
+console.log("value : " , mydatabase);
+
 console.log("Start of an awesome project");
+
