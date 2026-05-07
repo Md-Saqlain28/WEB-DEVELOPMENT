fea83e74e6bb6afd021a1856623567b15ff6660a
Mohammed Saqlain <mdsaqlain2486@gmail.com>
2026-05-07 20:02:19 +0530

    Wrote health check func using async await

--- ProjectManagement/src/controllers/healthcheck.controllers.js
+++ ProjectManagement/src/controllers/healthcheck.controllers.js
@@ -1,11 +1,24 @@
 import { ApiResponse } from "../utils/api_response.js";
+import { asyncHandler } from "../utils/async-handlers.js";
 
-const healthCheck = (req, res) => {
+/*
+const healthCheck = async (req, res, next) => {
   try {
+    const user = await getUserFromDB(); // Hypothetical function to check database connectivity
     res
       .status(200)
       .json(new ApiResponse(200, { message: "Server is running" }));
-  } catch (error) {}
+  } catch (error) {
+    next(error);
+  }
 };
+*/
+
+const healthCheck = asyncHandler(async (req, res) => {
+  // You can add any health check logic here, such as checking database connectivity
+  res
+  .status(200)
+  .json(new ApiResponse(200, { message: "Server is running" }));
+});
 
 export { healthCheck };
