c3f03501c579cfbab33cf8c560f0fe61d1a1ac68
Mohammed Saqlain <mdsaqlain2486@gmail.com>
2026-07-28 18:04:32 +0530

    Handled the error

--- ProjectManagement/src/index.js
+++ ProjectManagement/src/index.js
@@ -1,6 +1,10 @@
+import dns from "node:dns";
 import dotenv from "dotenv";
 import app from "./app.js";
 import connectDB from "./db/index.js";
+
+// Force Node to use Google and Cloudflare DNS servers for resolving MongoDB SRV records
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 dotenv.config({
   path: "./.env",
@@ -11,7 +15,7 @@ const port = process.env.PORT || 3000;
 connectDB()
   .then(() => {
     app.listen(port, () => {
-      console.log(`Example app listening on port https://localhost:${port}`);
+      console.log(`Example app listening on port http://localhost:${port}`);
     });
   })
   .catch((error) => {
