fe575cef76c6a63ee92bbacf31905e9a19f314d9
Mohammed Saqlain <mdsaqlain2486@gmail.com>
2026-07-31 18:46:11 +0530

    Wrote a function to login a user with tokens and cookies

--- ProjectManagement/src/controllers/auth.controller.js
+++ ProjectManagement/src/controllers/auth.controller.js
@@ -71,7 +71,51 @@ const registerUser = asyncHandler(async (req, res) => {
         )
 });
 
+const login = asyncHandler(async (req, res) => {
+    const {email, password, username} = req.body;
 
-export {registerUser};
+    if(!email){
+        throw new ApiError(400, "Email is required");
+    }
+
+    const user = await User.findOne({email});
+
+    if(!user){
+        throw new ApiError(404, "User does not exist");
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if(!isPasswordValid){
+        throw new ApiError(401, "Invalid user credentials");
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id);
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken  -emailVerificationToken -emailVerificationTokenExpiry");
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {user: loggedInUser, accessToken, refreshToken},
+                "User logged in successfully"
+            )
+        )
+       
+
+
+});
+
+
+export {registerUser, login};
 
 
