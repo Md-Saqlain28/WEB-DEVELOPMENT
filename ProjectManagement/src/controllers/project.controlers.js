f799116a5f1f9dfe9ba523687738414ebd7ae95c
Mohammed Saqlain <mdsaqlain2486@gmail.com>
2026-09-16 16:17:53 +0530

    Wrote the code to update member roles

--- ProjectManagement/src/controllers/project.controlers.js
+++ ProjectManagement/src/controllers/project.controlers.js
@@ -213,7 +213,40 @@ const getProjectMembers = asyncHandler(async (req, res) => {
 });
 
 const updateMemberRole = asyncHandler(async (req, res) => {
-    //test
+    const {projectId, userId} = req.params;
+    const {newRole} = req.body;
+
+    if(!AvailableRoles.includes(newRole)) {
+        throw new ApiError(400, "Invalid role");
+    }
+
+    let projectMember = await ProjectMember.findOne({
+        project: new mongoose.Types.ObjectId(projectId),
+        user: new mongoose.Types.ObjectId(userId)
+    });
+
+    if (!projectMember) {
+        throw new ApiError(404, "Project member not found");
+    }
+
+    projectMember = await ProjectMember.findByIdAndUpdate(
+        projectMember._id,
+        {
+            role: newRole  
+        },
+        {
+            new: true
+        } 
+    );
+
+    if (!projectMember) {
+        throw new ApiError(404, "Project member not found");
+    }
+
+
+    return res
+        .status(200)
+        .json(new ApiResponse(true, "Member role updated successfully", projectMember));
 });
 
 const deleteMember = asyncHandler(async (req, res) => {
