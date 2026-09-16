9be6e1fc1541e6b1bd7ffc1c2eda6412a4cdfa08
Mohammed Saqlain <Mohammed Saqlain>
2026-09-16 19:30:18 +0530

    Wrote the code for role based access

--- ProjectManagement/src/middlewares/auth.middleware.js
+++ ProjectManagement/src/middlewares/auth.middleware.js
@@ -1,4 +1,5 @@
 import {User} from '../models/user.model.js';
+import {ProjectMember} from '../models/projectmemeber.models.js';
 import {ApiError} from '../utils/api_error.js';
 import jwt from 'jsonwebtoken';
 import {asyncHandler} from '../utils/async-handlers.js';
@@ -23,3 +24,35 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
         throw new ApiError(401, "Invalid access token");
     }
 })
+
+export const validateProjectPermission = (roles = []) => {
+    asyncHandler(async (req, res, next) => {
+        const {projectId} = req.params;
+
+        if (!projectId) {
+            throw new ApiError(400, "Project ID is missing");
+        }
+
+        await ProjectMember.findOne({
+            project: new mongoose.Types.ObjectId(projectId),
+            user: new mongoose.Types.ObjectId(req.user._id),
+        })
+
+         if (!project) {
+            throw new ApiError(400, "Project not found");
+        }
+
+        const givenRole = project?.role
+        req.user.role = givenRole
+
+        if(!roles.includes(givenRole)){
+            throw new ApiError(
+                403,
+                "You do not have permission to perform this action"
+            )
+        }
+
+        next();
+
+    })
+}
