e39ae433224848964491074a2f4233847af2366e
Mohammed Saqlain <Mohammed Saqlain>
2026-09-17 19:17:53 +0530

    Wrote the code to getTask and createTask

--- ProjectManagement/src/controllers/task.controllers.js
+++ ProjectManagement/src/controllers/task.controllers.js
@@ -5,20 +5,74 @@ import {Subtask} from '../models/subtask.models.js';
 import { ApiResponse } from "../utils/api_response.js";
 import { ApiError } from "../utils/api_error.js";
 import { asyncHandler } from "../utils/async-handlers.js";
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import { AvailableUserRoles, UserRolesEnum } from '../utils/constants.js';
 
 
 const getTasks = asyncHandler(async (req , res) => {
-    //chai
+    const {projectId} = req.params;
+
+    const project = await Project.findById(projectId);
+
+    if(!project){
+        throw new ApiError(404, "Project not found");
+    }
+    
+    const Tasks = await Task.find({
+        project: new mongoose.Types.ObjectId(projectId),
+    }).populate("assignedTo", "avatar username fullName")
+
+     return res
+        .status(201)
+        .json(
+            new ApiResponse(201, Tasks, "Tasks fetched successfully")
+        )
+
+
 });
 
 const createTasks = asyncHandler(async (req , res) => {
-    //chai
+    const {title, description, assignedTo, status} = req.body;
+    const {projectId} = req.params;
+
+    const project = await Project.findById(projectId);
+
+    if(!project){
+        throw new ApiError(404, "Project not found");
+    }
+
+    const files = req.files || []
+
+    files.map((file) => {
+        return {
+            url: `${process.env.SERVER_URL}/images/${file.originalname}`,
+            mimeType: file.mimetype,
+            size: file.size
+        }
+    });
+
+    const task = await Task.create({
+        title,
+        description,
+        project: new mongoose.Types.ObjectId(projectId),
+        assinedTo: assignedTo ? new mongoose.Types.ObjectId(assignedTo): undefined,
+        status,
+        assignedBy: new mongo.Types.ObjectId(req.user._id),
+        attachments
+    });
+
+    return res
+        .status(201)
+        .json(
+            new ApiResponse(201, task, "Task created successfully")
+        )
+
+
 }); 
 
 const getTaskById = asyncHandler(async (req , res) => {
-    //chai
+    const {taskId} = req.params
+    
 }); 
 
 const updateTask = asyncHandler(async (req , res) => {
